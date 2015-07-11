305a61635a4fffb2eb1a72d562e55c7a92d1cf9f
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2015-07-11 17:27:44 +0100

    ImageUploadTest

---
+++ ImageUploadTest/WebFolder/index.waPage/scripts/index.js
@@ -0,0 +1,22 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var productEvent = {};	// @dataSource
+// @endregion// @endlock
+var dataInit = false; 
+// eventHandlers// @lock
+
+	productEvent.onCollectionChange = function productEvent_onCollectionChange (event)// @startlock
+	{// @endlock
+		if(dataInit == false)
+		{
+			sources.product.addNewElement();
+			
+		}
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("product", "onCollectionChange", productEvent.onCollectionChange, "WAF");
+// @endregion
+};// @endlock
